832fb7eeb95e216e7c01df0ef0359237d669c00a
AbdullahBhatti1987 <abdullah.bhatti345@gmail.com>
2024-12-12 19:26:35 +0500

    first commit

---
+++ src/content/data.js
@@ -0,0 +1,329 @@
+const data = {
+  header: {
+    logo: "https://cdn.vectorstock.com/i/500p/38/25/registered-trademark-logo-icon-copyright-mark-vector-36613825.jpg",
+    title: "Bhatti",
+    offer: {
+      message: " Sale Up To 50% Biggest Discounts. Hurry! Limited Perriod Offer",
+      slug: "/categories",
+      link: "#",
+    },
+
+    navbar: [
+      {
+        id: "1",
+        slug: "/home",
+        name: "Home",
+      },
+      {
+        id: "2",
+        slug: "/women",
+        name: "women",
+      },
+      {
+        id: "3",
+        slug: "/men",
+        name: "Men",
+      },
+      {
+        id: "4",
+        slug: "/babyCollection",
+        name: "Baby",
+      },
+      {
+        id: "5",
+        slug: "/pages",
+        name: "Pages",
+      },
+      {
+        id: "6",
+        slug: "/blog",
+        name: "Blog",
+      },
+      {
+        id: "7",
+        slug: "/contact",
+        name: "Contact",
+      },
+    ],
+    // icons: [
+    //   { icon: "<IoIosSearch />", name: "S", slug: "/search" },
+    //   { icon: "<AiOutlineUser />", name: "U", slug: "/user" },
+    //   { icon: "<IoCartOutline />", name: "C", slug: "/cart" },
+    // ],
+  },
+  banner: {
+    promotion: "50% OFF",
+    title: "Lorem ipsum dolor sit amet consectetur",
+    statement:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab velit ea alias maxime inventore aliquid, neque qui. Sed repudiandae omnis nemo voluptatem molestiae nesciunt qui explicabo tempora, quod nihil dicta?",
+    button: "SHOP NOW",
+    slug: "/categories",
+    images: [
+      "https://images.unsplash.com/photo-1601388352547-2802c6f32eb8?q=80&w=1963&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://images.unsplash.com/photo-1532879311112-62b7188d28ce?q=80&w=1871&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://images.unsplash.com/photo-1533282960533-51328aa49826?q=80&w=1842&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://images.unsplash.com/photo-1593161499316-0cd67918ab6a?q=80&w=1902&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://images.unsplash.com/photo-1547039996-61c1135690c0?q=80&w=1989&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://images.unsplash.com/photo-1589271755419-b813a577ad42?q=80&w=1809&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    ],
+  },
+  categories: [
+    {
+      id: 1,
+      slug: "/men",
+      name: "Men",
+      image:
+        "https://cdnewmoda.expatwoman.com/s3fs-public/styles/width_420/public/GettyImages-9053ddd66210.jpg?itok=I-fL8YcT",
+    },
+    {
+      id: 2,
+      slug: "/women",
+      name: "Women",
+      image: "https://www.economy.pk/wp-content/uploads/2020/11/Fashion-Icon.jpg",
+    },
+    {
+      id: 3,
+      slug: "/baby",
+      name: "Baby",
+      image: "https://i.pinimg.com/736x/a9/b4/b3/a9b4b3d1a6503664745785947341e2c7.jpg",
+    },
+  ],
+  slider: {
+    heading: "Trending This Week",
+
+    data: [
+      {
+        id: "1",
+        slug: "/mens/1",
+        name: "501® '93 Straight Fit Selvedge Men's Jeans",
+        category: "Men's",
+        price: 148,
+        image:
+          "https://lsco.scene7.com/is/image/lsco/798300355-dynamic1-pdp?fmt=webp&qlt=70&resMode=sharp2&fit=crop,1&op_usm=0.6,0.6,8&wid=320&hei=427",
+      },
+      {
+        id: "2",
+        slug: "/mens/2",
+        name: "502® '93 Straight Fit Selvedge Men's Jeans",
+        category: "Men's",
+        price: 105,
+        image:
+          "https://lsco.scene7.com/is/image/lsco/005013671-dynamic1-pdp?fmt=webp&qlt=70&resMode=sharp2&fit=crop,1&op_usm=0.6,0.6,8&wid=560&hei=747",
+      },
+      {
+        id: "3",
+        slug: "/mens/3",
+        name: "503® '93 Straight Fit Selvedge Men's Jeans",
+        category: "Men's",
+        price: 132,
+        image:
+          "https://lsco.scene7.com/is/image/lsco/085130934-dynamic1-pdp?fmt=webp&qlt=70&resMode=sharp2&fit=crop,1&op_usm=0.6,0.6,8&wid=560&hei=747",
+      },
+      {
+        id: "4",
+        slug: "/mens/4",
+        name: "504® '93 Straight Fit Selvedge Men's Jeans",
+        category: "Men's",
+        price: 198,
+        image:
+          "https://lsco.scene7.com/is/image/lsco/005500216-front-pdp?fmt=webp&qlt=70&resMode=sharp2&fit=crop,1&op_usm=0.6,0.6,8&wid=560&hei=747",
+      },
+      {
+        id: "5",
+        slug: "/mens/5",
+        name: "505® '93 Straight Fit Selvedge Men's Jeans",
+        category: "Men's",
+        price: 165,
+        image:
+          "https://lsco.scene7.com/is/image/lsco/798300356-dynamic1-pdp?fmt=webp&qlt=70&resMode=sharp2&fit=crop,1&op_usm=0.6,0.6,8&wid=560&hei=747",
+      },
+      {
+        id: "6",
+        slug: "/mens/6",
+        name: "506® '93 Straight Fit Selvedge Men's Jeans",
+        category: "Men's",
+        price: 148,
+        image:
+          "https://lsco.scene7.com/is/image/lsco/005500216-front-pdp?fmt=webp&qlt=70&resMode=sharp2&fit=crop,1&op_usm=0.6,0.6,8&wid=560&hei=747",
+      },
+      {
+        id: "7",
+        slug: "/mens/7",
+        name: "507® '93 Straight Fit Selvedge Men's Jeans",
+        category: "Men's",
+        price: 148,
+        image:
+          "https://lsco.scene7.com/is/image/lsco/A47500020-dynamic1-pdp?fmt=webp&qlt=70&resMode=sharp2&fit=crop,1&op_usm=0.6,0.6,8&wid=560&hei=747",
+      },
+      {
+        id: "8",
+        slug: "/mens/8",
+        name: "508® '93 Straight Fit Selvedge Men's Jeans",
+        category: "Men's",
+        price: 148,
+        image:
+          "https://lsco.scene7.com/is/image/lsco/181810866-dynamic1-pdp?fmt=webp&qlt=70&resMode=sharp2&fit=crop,1&op_usm=0.6,0.6,8&wid=560&hei=747",
+      },
+      {
+        id: "9",
+        slug: "/mens/8",
+        name: "509® '93 Straight Fit Selvedge Men's Jeans",
+        category: "Men's",
+        price: 148,
+        image:
+          "https://lsco.scene7.com/is/image/lsco/005013649-dynamic1-pdp?fmt=webp&qlt=70&resMode=sharp2&fit=crop,1&op_usm=0.6,0.6,8&wid=560&hei=747",
+      },
+    ],
+  },
+  blogs: [
+    {
+      id: 1,
+      title: "Title-1",
+      message:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio quia nostrum ad dolores inventore nesciunt reiciendis tempore, omnis blanditiis eos iusto provident! Perspiciatis, sunt incidunt illum fugiat excepturi ducimus beatae!",
+      image: "https://upload.wikimedia.org/wikipedia/en/0/02/DotBlog_domain_logo.png",
+    },
+    {
+      id: 2,
+      title: "Title-2",
+      message:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio quia nostrum ad dolores inventore nesciunt reiciendis tempore, omnis blanditiis eos iusto provident! Perspiciatis, sunt incidunt illum fugiat excepturi ducimus beatae!",
+        image: "https://upload.wikimedia.org/wikipedia/en/0/02/DotBlog_domain_logo.png",
+    },
+    {
+      id: 3,
+      title: "Title-3",
+      message:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio quia nostrum ad dolores inventore nesciunt reiciendis tempore, omnis blanditiis eos iusto provident! Perspiciatis, sunt incidunt illum fugiat excepturi ducimus beatae!",
+        image: "https://upload.wikimedia.org/wikipedia/en/0/02/DotBlog_domain_logo.png",
+    },
+    {
+      id: 4,
+      title: "Title-4",
+      message:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio quia nostrum ad dolores inventore nesciunt reiciendis tempore, omnis blanditiis eos iusto provident! Perspiciatis, sunt incidunt illum fugiat excepturi ducimus beatae!",
+        image: "https://upload.wikimedia.org/wikipedia/en/0/02/DotBlog_domain_logo.png",
+    },
+    {
+      id: 5,
+      title: "Title-5",
+      message:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio quia nostrum ad dolores inventore nesciunt reiciendis tempore, omnis blanditiis eos iusto provident! Perspiciatis, sunt incidunt illum fugiat excepturi ducimus beatae!",
+        image: "https://upload.wikimedia.org/wikipedia/en/0/02/DotBlog_domain_logo.png",
+    },
+  ],
+  latest: [
+    {
+      id: 1,
+      category: "abc",
+      title: "ABC",
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio quia nostrum ad dolores inventore nesciunt reiciendis tempore, omnis blanditiis eos iusto provident! Perspiciatis, sunt incidunt illum fugiat excepturi ducimus beatae!",
+      slug: "Read More",
+    },
+    {
+      id: 2,
+      category: "def",
+      title: "DEF",
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio quia nostrum ad dolores inventore nesciunt reiciendis tempore, omnis blanditiis eos iusto provident! Perspiciatis, sunt incidunt illum fugiat excepturi ducimus beatae!",
+      slug: "Read More",
+    },
+    {
+      id: 3,
+      category: "ghi",
+      title: "GHI",
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio quia nostrum ad dolores inventore nesciunt reiciendis tempore, omnis blanditiis eos iusto provident! Perspiciatis, sunt incidunt illum fugiat excepturi ducimus beatae!",
+      slug: "Read More",
+    },
+  ],
+  procedure: [
+    {
+      id: 1,
+      icon: "icon",
+      title: "Fast & Free Delivery",
+      message: "Free delivery on all orders",
+    },
+    {
+      id: 2,
+      icon: "icon",
+      title: "Secure Payment",
+      message: "Free delivery on all orders",
+    },
+    {
+      id: 3,
+      icon: "icon",
+      title: "Money Back Guarantee",
+      message: "Free delivery on all orders",
+    },
+    {
+      id: 4,
+      icon: "icon",
+      title: "Online Support",
+      message: "Free delivery on all orders",
+    },
+  ],
+  newLetter: {
+    title: "Subscribe Newsletter",
+    message: "Subscribe newsletter to get 5% on all products.",
+    icons: [
+      { id: 1, icon: "Facebook", slug: "/facebook" },
+      { id: 1, icon: "Instagram", slug: "/instagram" },
+      { id: 1, icon: "youtube", slug: "/youtube" },
+    ],
+  },
+  footer: {
+    image: "",
+    heading: "",
+    categories: [
+      {
+        id: 1,
+        listName: "Men Shop",
+        category: [
+          { id: 1, listItem: "listItem1", slug: "/nav1" },
+          { id: 2, listItem: "listItem2", slug: "/nav2" },
+          { id: 3, listItem: "listItem3", slug: "/nav3" },
+          { id: 4, listItem: "listItem4", slug: "/nav4" },
+          { id: 5, listItem: "listItem5", slug: "/nav5" },
+        ],
+      },
+      {
+        id: 2,
+        listName: "Women Shop",
+        category: [
+          { id: 1, listItem: "listItem11", slug: "/nav1" },
+          { id: 2, listItem: "listItem12", slug: "/nav2" },
+          { id: 3, listItem: "listItem13", slug: "/nav3" },
+          { id: 4, listItem: "listItem14", slug: "/nav4" },
+          { id: 5, listItem: "listItem15", slug: "/nav5" },
+        ],
+      },
+      {
+        id: 3,
+        listName: "Baby Collection",
+        category: [
+          { id: 1, listItem: "listItem21", slug: "/nav1" },
+          { id: 2, listItem: "listItem22", slug: "/nav2" },
+          { id: 3, listItem: "listItem23", slug: "/nav3" },
+          { id: 4, listItem: "listItem24", slug: "/nav4" },
+          { id: 5, listItem: "listItem25", slug: "/nav5" },
+        ],
+      },
+      {
+        id: 1,
+        listName: "Quick Links",
+        category: [
+          { id: 1, listItem: "listItem21", slug: "/nav1" },
+          { id: 2, listItem: "listItem22", slug: "/nav2" },
+          { id: 3, listItem: "listItem23", slug: "/nav3" },
+          { id: 4, listItem: "listItem24", slug: "/nav4" },
+          { id: 5, listItem: "listItem25", slug: "/nav5" },
+        ],
+      },
+    ],
+    copyright: "Copyright ©2025 All rights reserved | This template is made with by Abdullah Bhatti",
+  },
+};
+
+export default data;
